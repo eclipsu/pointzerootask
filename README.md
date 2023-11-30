24cc465e907f3e8a56a24985529d303c68265207
Your Name <you@example.com>
2023-11-30 20:14:36 +0545

    New readme

--- README.md
+++ README.md
@@ -1,8 +1,77 @@
-# React + Vite
+# Internship Task - Data Rendering with ReactJS
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+## Project Overview
 
-Currently, two official plugins are available:
+This project is a solution to the data rendering challenge provided by .pointzero for the internship task. The goal was to create a React app that fetches and displays product information from the [Fake Store API](https://fakestoreapi.com/products). The app includes a product listing page and dynamic routes for individual product details.
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+## Technologies Used
+
+- React
+- Vite
+- Axios
+
+## Features
+
+- Product Listing Page
+- Dynamic Route for Individual Product Details
+- Utilization of Modern React Practices (e.g., useEffect, useState hooks)
+- Component-Based Structure for Code Organization
+
+## Getting Started
+
+### Prerequisites
+
+- Node.js and npm installed
+
+### Installation
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/your-username/your-repo.git
+   ```
+
+2. Navigate to the project directory:
+
+```bash
+cd your-repo
+```
+
+3. Install dependencies:
+
+```bash
+npm install
+```
+
+4. Run the development server:
+
+```bash
+npm run dev
+```
+
+    Open your browser and visit http://localhost:3000 to view the app.
+
+# Folder Structure
+
+```lua
+/src
+|-- components
+|   |-- ProductList.jsx
+|   |-- Products
+|       |-- Product.jsx
+|       |-- ProductItem.jsx
+|-- pages
+|   |-- Home.jsx
+|-- assets
+|   |-- react.svg
+|-- styles
+|-- App.jsx
+|-- App.css
+|-- index.css
+|-- main.jsx
+
+```
+
+# [Live demo here](https://pointzerootask.vercel.app/)
+
+I hosted the live demo on vercel.
--- src/components/Products/Product.jsx
+++ src/components/Products/Product.jsx
@@ -1,31 +1,59 @@
 // Product.jsx
 import { Link } from "react-router-dom";
 import "./Product.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import ReactStars from "react-stars";
 
-const Product = ({ id, title, price, description, category, image, rating }) => {
+const Product = ({ id }) => {
+  const [productData, setProductData] = useState({});
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`https://fakestoreapi.com/products/${id}`);
+        setProductData(response.data);
+      } catch (error) {
+        setError("Error fetching data from the API");
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, [id]);
+
+  const { title, price, description, category, image, rating } = productData;
+
   const starProps = {
     size: 30,
-    value: rating.rate,
+    value: rating?.rate || 0,
     edit: false,
   };
+
   return (
-    <Link to={`/product/${id}`} className="product-link">
-      <div className="product-item">
-        <img className="product-image" src={image} alt={title} />
-        <div className="product-details">
-          <h2 className="product-title">{title}</h2>
-          <p className="product-description">{description}</p>
-          <p className="product-price">${price}</p>
-          <p className="product-category">{category}</p>
-          {rating && ( // Check if rating exists
-            <div className="product-rating">
-              <ReactStars {...starProps} /> ({rating.count})
+    <>
+      {error ? (
+        <div className="error-message">{error}</div>
+      ) : (
+        <Link to={`/product/${id}`} className="product-link">
+          <div className="product-item">
+            <img className="product-image" src={image} alt={title} />
+            <div className="product-details">
+              <h2 className="product-title">{title}</h2>
+              <p className="product-description">{description}</p>
+              <p className="product-price">Rs. {price}</p>
+              <p className="product-category">{category}</p>
+              {rating && (
+                <div className="product-rating">
+                  <ReactStars {...starProps} /> ({rating.count})
+                </div>
+              )}
             </div>
-          )}
-        </div>
-      </div>
-    </Link>
+          </div>
+        </Link>
+      )}
+    </>
   );
 };
 
